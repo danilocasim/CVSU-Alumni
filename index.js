6f40444e30ce4e3e0510231ea080d8324a170a67
danilocasim <danilocasim174@gmail.com>
2024-01-27 15:35:14 +0800

    revert

--- index.js
+++ index.js
@@ -64,7 +64,6 @@ const navSlide = () => {
           link.style.animation = `navLinkFade 0.5s ease forwards ${
             index / 7 + 0.3
           }s`;
-        link.style.visibility = "visible";
       }
     });
 
